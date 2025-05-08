0b285439ed58d0b650ab2ecd1de271d51659269c
Haryanto Nugroho <nharyanto2012@gmail.com>
2025-05-08 08:32:30 +0700

    Update and rename Hello.java to 01 - Hello.java

--- 01 - Hello.java
+++ 01 - Hello.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package project1;
 
 /**
  *
@@ -13,7 +12,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("===================================");
         System.out.println("    Hello, Saya Haryanto Nugroho   ");
-        System.out.println("           NIM 22110602            ");
         System.out.println("Sedang memulai Belajar Program Java");
         System.out.println("===================================");
     }
